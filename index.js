5df9a155855a63422d309c842bb82bf3b7bc81ea
曹苏 <caosu@51box.cn>
2017-08-18 20:04:44 +0800

    服务器测试

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('build/index.html');
